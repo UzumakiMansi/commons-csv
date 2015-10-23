353b92677d6e08c6bb54a6e59e5923306e586ad3
mbabbar <mbabbar@ncsu.edu>
2015-10-23 16:02:11 -0700

    pre-commit hook

--- check_key.py
+++ check_key.py
@@ -10,11 +10,11 @@ for root, dirs, files in os.walk("./"):
             for line in OpenFile:
                 do = re.search("[a-zA-Z0-9]{63,65}", line)
                 if do:
-                    print ("Found DigitalOcean security Token: %s, hence rejecting the build! \nBetter luck next time!\n" %(line))
+                    print ("Found DigitalOcean security Token: %s\nRejecting the commit! \nBetter luck next time!\n" %(line))
                     sys.exit(-1)
                 aws = re.search("AKI[a-zA-Z0-9]{17,18}", line)
                 if aws:
-                    print ("Found AWS security Token: %s, hence rejecting the build! \nBetter luck next time!\n" %(line))
+                    print ("Found AWS security Token: %s\nRejecting the commit! \nBetter luck next time!\n" %(line))
                     sys.exit(-1)
         if file.endswith(".pem"):
             print file

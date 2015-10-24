6c6d053e845f82409e9fd192c17822556d8f26f5
mbabbar <mbabbar@ncsu.edu>
2015-10-23 17:13:51 -0700

    Added check for Token->pre-commit hook

---
+++ check_key.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import re
+import os
+import sys
+
+for root, dirs, files in os.walk("./"):
+    for file in files:
+        if os.path.isfile(file):
+            OpenFile = open(file, 'r')
+            for line in OpenFile:
+                do = re.search("[a-zA-Z0-9]{63,65}", line)
+                if do:
+                    print ("Found DigitalOcean security Token: %s\nRejecting the commit! \nBetter luck next time!\n" %(line))
+                    sys.exit(-1)
+                aws = re.search("AKI[a-zA-Z0-9]{17,18}", line)
+                if aws:
+                    print ("Found AWS security Token: %s\nRejecting the commit! \nBetter luck next time!\n" %(line))
+                    sys.exit(-1)
+        if file.endswith(".pem"):
+            print file
+            sys.exit(-1)
